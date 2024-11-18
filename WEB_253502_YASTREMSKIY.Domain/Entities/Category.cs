fcee12cce5274dc6e3264710c1ad0fcce2b25d55
Yaroslav Yastremskiy <118111412+whiteprincewithobsession@users.noreply.github.com>
2024-11-18 16:45:51 +0300

    Delete WEB_253502_YASTREMSKIY.Domain directory

--- WEB_253502_YASTREMSKIY.Domain/Entities/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WEB_253502_YASTREMSKIY.Domain.Entities
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
-        public string NormalizedName { get; set; }
-    }
-}
